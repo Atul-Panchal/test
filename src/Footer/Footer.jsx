894501be0a609290e473aba0db7f3595b218c000
Atul Panchal <83485794+Atul-Panchal@users.noreply.github.com>
2023-05-21 20:21:31 +0530

    Add files via upload

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+  return (
+    <div className='f'>
+         <div className="footer">
+        <p>Contact me on social media</p>
+
+        <div className="icons">
+          
+        </div>
+        <div className="cpr">
+            <h6> Designed by Atul</h6>
+                <h6>All rights Reserved</h6>
+        </div>
+
+        <div className='copyRight'>All Rights Reserved</div>
+    </div>
+    </div>
+  )
+}
+
+export default Footer
